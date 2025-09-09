1a0ffced061f39e16beb13efb7d3a2246edb3d0e
Zaynab Ashrafi <zynbashrfi@gmail.com>
2025-09-09 19:01:23 +0330

    Initial commit: Set up project structure and config files

---
+++ core.py
@@ -0,0 +1,17 @@
+import json
+import datetime
+
+def load_expenses():
+    pass
+
+def save_expenses():
+    pass
+
+def add_expense():
+    pass
+
+def delete_expense():
+    pass
+
+def get_summary():
+    pass
